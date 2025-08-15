2077b3a65a339ae1fcf6d0f8425a8eb7c5e2328b
Ian-Protocol <ipascal@live.ca>
2025-08-15 02:35:16 -0500

    Add content components to Basic Info

--- src/pages/BasicInfo.tsx
+++ src/pages/BasicInfo.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Text } from '../components/Text/Text';
+import React from "react";
+import styled from "styled-components";
+import { Text } from "../components/Text/Text";
 
 const Container = styled.div`
   padding: 20px;
@@ -23,14 +23,42 @@ const Subtitle = styled.h2`
   margin-bottom: 15px;
 `;
 
+const Content = styled.div`
+    font-size: 16pt;
+    color: #444;
+    line-height: 1.5;
+`;
+
+const ContentTitle = styled.h3`
+    font-size: 18pt;
+    color: #555;
+    margin-top: 20px;
+    margin-bottom: 10px;
+`;
+
 export const BasicInfo = () => {
   return (
     <Container>
       <Header>
         <Title>Ian Pascal</Title>
         <Subtitle>Full Stack Web Developer</Subtitle>
-        <Text content="I like turtles." />
+        <Text content="Do I need anything here?" />
       </Header>
+
+      <Content>
+        <ContentTitle>About Me</ContentTitle>
+        <Text content="I have a passion for technology and solving problems with code. Computers are cool." />
+      </Content>
+
+      <Content>
+        <ContentTitle>Interests</ContentTitle>
+        <Text content="In my spare time, I enjoy hiking, exploring new technologies, playing video games and Magic: The Gathering, and working on personal projects - all of which are technological in nature." />
+      </Content>
+
+      <Content>
+        <ContentTitle>Education</ContentTitle>
+        <Text content="I (hopefully) have a diploma in Full Stack Web Development from Red River College Polytechnic." />
+      </Content>
     </Container>
   );
 };
