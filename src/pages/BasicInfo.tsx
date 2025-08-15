3e365790b3846632a43ba856c6f8d72d86d2cb81
Ian-Protocol <ipascal@live.ca>
2025-08-15 03:02:01 -0500

    Switched to using Card component for Basic Info

--- src/pages/BasicInfo.tsx
+++ src/pages/BasicInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Text } from "../components/Text/Text";
+import { Card } from "../components/Card/Card";
 
 const Container = styled.div`
   padding: 20px;
@@ -17,48 +18,39 @@ const Title = styled.h1`
   margin-bottom: 15px;
 `;
 
-const Subtitle = styled.h2`
-  font-size: 20pt;
-  background-color: #666;
-  margin-bottom: 15px;
-`;
+// const Subtitle = styled.h2`
+//   font-size: 20pt;
+//   background-color: #666;
+//   margin-bottom: 15px;
+// `;
 
-const Content = styled.div`
-  font-size: 16pt;
-  background-color: #444;
-  line-height: 1.5;
-`;
+// const Content = styled.div`
+//   font-size: 16pt;
+//   background-color: #444;
+//   line-height: 1.5;
+//   padding-bottom: 15px;
+// `;
 
-const ContentTitle = styled.h3`
-  font-size: 18pt;
-  background-color: #555;
-  margin-top: 20px;
-  margin-bottom: 10px;
-`;
+// const ContentTitle = styled.h3`
+//   font-size: 18pt;
+//   background-color: #555;
+//   margin-top: 10px;
+//   margin-bottom: 10px;
+// `;
 
 export const BasicInfo = () => {
   return (
     <Container>
       <Header>
         <Title>Ian Pascal</Title>
-        <Subtitle>Full Stack Web Developer</Subtitle>
-        <Text content="Do I need anything here?" />
+        <Text content="Full Stack Web Developer" />
       </Header>
 
-      <Content>
-        <ContentTitle>About Me</ContentTitle>
-        <Text content="I have a passion for technology and solving problems with code. Computers are cool." />
-      </Content>
+      <Card title="About Me" content="I have a passion for technology and solving problems with code. Computers are cool." />
 
-      <Content>
-        <ContentTitle>Interests</ContentTitle>
-        <Text content="In my spare time, I enjoy hiking, exploring new technologies, playing video games and Magic: The Gathering, and working on personal projects - all of which are technological in nature." />
-      </Content>
+      <Card title="Interests" content="In my spare time, I enjoy hiking, exploring new technologies, playing video games and Magic: The Gathering, and working on personal projects - all of which are technological in nature." />
 
-      <Content>
-        <ContentTitle>Education</ContentTitle>
-        <Text content="I (hopefully) have a diploma in Full Stack Web Development from Red River College Polytechnic." />
-      </Content>
+      <Card title="Education" content="I (hopefully) have a diploma in Full Stack Web Development from Red River College Polytechnic." />
     </Container>
   );
 };
